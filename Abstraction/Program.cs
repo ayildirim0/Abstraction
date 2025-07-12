7e41e49bd3a7011e96ff35f8139f6b5e2ec412ce
bmade <bmade@ADEM>
2025-07-13 00:10:50 +0300

    Add project files.

---
+++ Abstraction/Program.cs
@@ -0,0 +1,19 @@
+﻿using Abstraction;
+
+Work work = new Work()
+{
+    Name = "Adem",
+    SurName = "Yıldırım",
+    Department = "Yazılım Geliştirici"
+};
+// Görev metodu çağrılıyor
+work.Duty();
+
+Work work1 = new Work()
+{
+    Name = "Elif",
+    SurName = "Kaya",
+    Department = "Satış Temsilcisi"
+};
+// Görev metodu çağrılıyor
+work1.Duty();
---
+++ Abstraction/Worker.cs
@@ -0,0 +1,22 @@
+﻿using System.Threading.Tasks;
+
+namespace Abstraction
+{
+    public abstract class Worker
+    {
+        public string Name { get; set; }
+        public string SurName { get; set; }
+        public string Department { get; set; }
+        public abstract void Duty();// Soyut görev metodu
+    }
+    public class Work : Worker 
+    {
+        public override void Duty()
+        {
+            Console.WriteLine("Lütfen yaptığınız işi giriniz...");
+            string task = Console.ReadLine();
+
+            Console.WriteLine($"{Name} {SurName} şirketin {Department} departmanında {task} olarak çalışıyorum");
+        }
+    }
+}
